ca42f299ef8da1f4f5184691ecfcf83f7bb47630
Aditya-Coomar <adi.coomar04@gmail.com>
2025-01-17 05:53:25 +0530

    chore: change position for mute/unmute and play/pause

--- src/components/vertical-carousel.tsx
+++ src/components/vertical-carousel.tsx
@@ -127,8 +127,6 @@ const VerticalCarousel = () => {
     setIsMuted(!isMuted);
   };
 
-  console.log(items);
-
   return (
     <div className="h-full overflow-y-scroll scroll-snap-y scroll-snap-mandatory">
       {items.map((item: CarouselItemProps, index: number) => (
@@ -165,26 +163,6 @@ const VerticalCarousel = () => {
                     </Dialog>
                   </div>
                 </div>
-                <button
-                  title="video-mute-unmute"
-                  type="button"
-                  onClick={toggleMute}
-                  className="tracking-wide font-medium p-3"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    height="24px"
-                    viewBox="0 -960 960 960"
-                    width="24px"
-                    fill="#CCCCCC"
-                  >
-                    {isMuted ? (
-                      <path d="M671-177q-11 7-22 13t-23 11q-15 7-30.5 0T574-176q-6-15 1.5-29.5T598-227q4-2 7.5-4t7.5-4L480-368v111q0 27-24.5 37.5T412-228L280-360H160q-17 0-28.5-11.5T120-400v-160q0-17 11.5-28.5T160-600h88L84-764q-11-11-11-28t11-28q11-11 28-11t28 11l680 680q11 11 11 28t-11 28q-11 11-28 11t-28-11l-93-93Zm89-304q0-83-44-151.5T598-735q-15-7-22-21.5t-2-29.5q6-16 21.5-23t31.5 0q97 43 155 131t58 197q0 33-6 65.5T817-353q-8 22-24.5 27.5t-30.5.5q-14-5-22.5-18t-.5-30q11-26 16-52.5t5-55.5ZM591-623q33 21 51 63t18 80v10q0 5-1 10-2 13-14 17t-22-6l-51-51q-6-6-9-13.5t-3-15.5v-77q0-12 10.5-17.5t20.5.5Zm-201-59q-6-6-6-14t6-14l22-22q19-19 43.5-8.5T480-703v63q0 14-12 19t-22-5l-56-56Z" />
-                    ) : (
-                      <path d="M760-481q0-83-44-151.5T598-735q-15-7-22-21.5t-2-29.5q6-16 21.5-23t31.5 0q97 43 155 131.5T840-481q0 108-58 196.5T627-153q-16 7-31.5 0T574-176q-5-15 2-29.5t22-21.5q74-34 118-102.5T760-481ZM280-360H160q-17 0-28.5-11.5T120-400v-160q0-17 11.5-28.5T160-600h120l132-132q19-19 43.5-8.5T480-703v446q0 27-24.5 37.5T412-228L280-360Zm380-120q0 42-19 79.5T591-339q-10 6-20.5.5T560-356v-250q0-12 10.5-17.5t20.5.5q31 25 50 63t19 80Z" />
-                    )}
-                  </svg>
-                </button>
               </div>
               <div className="flex flex-col gap-8 items-center justify-end">
                 <div className="flex justify-between items-end w-full">
@@ -210,7 +188,8 @@ const VerticalCarousel = () => {
                         }`}
                         onClick={() => {
                           const newItems = [...items];
-                          newItems[index].subscribedByUser = !item.subscribedByUser;
+                          newItems[index].subscribedByUser =
+                            !item.subscribedByUser;
                           setItems(newItems);
                         }}
                       >
@@ -236,6 +215,53 @@ const VerticalCarousel = () => {
                   </div>
                   <div className="flex flex-col justify-end items-center gap-6 bg-transparent px-2">
                     <button
+                      title="video-mute-unmute"
+                      type="button"
+                      onClick={toggleMute}
+                      className="tracking-wide font-medium"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="32px"
+                        viewBox="0 -960 960 960"
+                        width="32px"
+                        fill="#CCCCCC"
+                      >
+                        {isMuted ? (
+                          <path d="M671-177q-11 7-22 13t-23 11q-15 7-30.5 0T574-176q-6-15 1.5-29.5T598-227q4-2 7.5-4t7.5-4L480-368v111q0 27-24.5 37.5T412-228L280-360H160q-17 0-28.5-11.5T120-400v-160q0-17 11.5-28.5T160-600h88L84-764q-11-11-11-28t11-28q11-11 28-11t28 11l680 680q11 11 11 28t-11 28q-11 11-28 11t-28-11l-93-93Zm89-304q0-83-44-151.5T598-735q-15-7-22-21.5t-2-29.5q6-16 21.5-23t31.5 0q97 43 155 131t58 197q0 33-6 65.5T817-353q-8 22-24.5 27.5t-30.5.5q-14-5-22.5-18t-.5-30q11-26 16-52.5t5-55.5ZM591-623q33 21 51 63t18 80v10q0 5-1 10-2 13-14 17t-22-6l-51-51q-6-6-9-13.5t-3-15.5v-77q0-12 10.5-17.5t20.5.5Zm-201-59q-6-6-6-14t6-14l22-22q19-19 43.5-8.5T480-703v63q0 14-12 19t-22-5l-56-56Z" />
+                        ) : (
+                          <path d="M760-481q0-83-44-151.5T598-735q-15-7-22-21.5t-2-29.5q6-16 21.5-23t31.5 0q97 43 155 131.5T840-481q0 108-58 196.5T627-153q-16 7-31.5 0T574-176q-5-15 2-29.5t22-21.5q74-34 118-102.5T760-481ZM280-360H160q-17 0-28.5-11.5T120-400v-160q0-17 11.5-28.5T160-600h120l132-132q19-19 43.5-8.5T480-703v446q0 27-24.5 37.5T412-228L280-360Zm380-120q0 42-19 79.5T591-339q-10 6-20.5.5T560-356v-250q0-12 10.5-17.5t20.5.5q31 25 50 63t19 80Z" />
+                        )}
+                      </svg>
+                    </button>
+                    <button
+                      type="button"
+                      title="play-pause"
+                      onClick={() => togglePlayPause(index)}
+                    >
+                      {playingStates[index] ? (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="32px"
+                          viewBox="0 -960 960 960"
+                          width="32px"
+                          fill="#CCCCCC"
+                        >
+                          <path d="M640-200q-33 0-56.5-23.5T560-280v-400q0-33 23.5-56.5T640-760q33 0 56.5 23.5T720-680v400q0 33-23.5 56.5T640-200Zm-320 0q-33 0-56.5-23.5T240-280v-400q0-33 23.5-56.5T320-760q33 0 56.5 23.5T400-680v400q0 33-23.5 56.5T320-200Z" />
+                        </svg>
+                      ) : (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          height="32px"
+                          viewBox="0 -960 960 960"
+                          width="32px"
+                          fill="#CCCCCC"
+                        >
+                          <path d="M320-273v-414q0-17 12-28.5t28-11.5q5 0 10.5 1.5T381-721l326 207q9 6 13.5 15t4.5 19q0 10-4.5 19T707-446L381-239q-5 3-10.5 4.5T360-233q-16 0-28-11.5T320-273Z" />
+                        </svg>
+                      )}
+                    </button>
+                    <button
                       className="flex flex-col gap-1 items-center justify-center"
                       title="like-unlike-toggle"
                       type="button"
@@ -243,7 +269,6 @@ const VerticalCarousel = () => {
                         const newItems = [...items];
                         if (item.likedByUser) {
                           newItems[index].likes -= 1;
-
                         } else {
                           newItems[index].likes += 1;
                         }
